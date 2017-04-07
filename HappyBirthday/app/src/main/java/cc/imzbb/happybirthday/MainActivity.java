0936b84261e88312584cb25d05ad61d746bff310
ZHENG Bangbo <zbbzucc@gmail.com>
2017-04-07 19:50:53 +0800

    修改到可以真正全屏的版本

--- HappyBirthday/app/src/main/java/cc/imzbb/happybirthday/MainActivity.java
+++ HappyBirthday/app/src/main/java/cc/imzbb/happybirthday/MainActivity.java
@@ -1,16 +1,24 @@
 package cc.imzbb.happybirthday;
 
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private View myview;
+//    Window window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        //熄灭地下导航栏的灯（白色图案消失）
+//        window = getWindow();
+//        WindowManager.LayoutParams params = window.getAttributes();
+//        params.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+//        window.setAttributes(params);
 
         setContentView(R.layout.activity_main);
 
@@ -19,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
 
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+//        myview = findViewById(R.id.textView);
+//        myview.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
 //        //去掉信息栏
 //        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # studyjamscn
 在 http://www.studyjamscn.com 学习 Android 的一些 project 。
+
+## HappyBirthday
+这个项目做过的笔记 http://www.studyjamscn.com/thread-18179-1-1.html
