3c2bf4c3275d0865b47dfcb72993c4ba098ecf55
zbbloveplay <zbbzucc@gmail.com>
2017-04-13 22:03:58 +0800

    记分器添加回退功能

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -5,17 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     *   +1 +2 +3
+     * A  1  2  3
+     * B  4  5  6
+     * */
+    int undo_flag = 0;
+
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+
+    Button bt_reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_reset =(Button) findViewById(R.id.bt_reset);
+        bt_reset.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                //Toast.makeText(MainActivity.this,"正在识别二维码", Toast.LENGTH_SHORT).show();
+                scoreTeamA = 0;
+                scoreTeamB = 0;
+                displayForTeamA(scoreTeamA);
+                displayForTeamB(scoreTeamB);
+
+                return true;
+            }
+        });
     }
 
     /**
@@ -24,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     public void addOneForTeamA(View v) {
         scoreTeamA ++;
         displayForTeamA(scoreTeamA);
+        undo_flag = 1;
     }
 
     /**
@@ -32,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     public void addTwoForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
+        undo_flag = 2;
     }
 
     /**
@@ -40,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
+        undo_flag = 3;
     }
 
     /**
@@ -56,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
     public void addOneForTeamB(View v) {
         scoreTeamB ++;
         displayForTeamB(scoreTeamB);
+        undo_flag = 4;
     }
 
     /**
@@ -64,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
+        undo_flag = 5;
     }
 
     /**
@@ -72,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
+        undo_flag = 6;
     }
 
     /**
@@ -82,11 +112,25 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void resetScore(View v){
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+    public void resetScore(View v) {
+        switch (undo_flag){
+            case 1 :
+                scoreTeamA = scoreTeamA - 1;displayForTeamA(scoreTeamA);undo_flag = 0;break;
+            case 2 :
+                scoreTeamA = scoreTeamA - 2;displayForTeamA(scoreTeamA);undo_flag = 0;break;
+            case 3 :
+                scoreTeamA = scoreTeamA - 3;displayForTeamA(scoreTeamA);undo_flag = 0;break;
+            case 4 :
+                scoreTeamB = scoreTeamB - 1;displayForTeamB(scoreTeamB);undo_flag = 0;break;
+            case 5 :
+                scoreTeamB = scoreTeamB - 2;displayForTeamB(scoreTeamB);undo_flag = 0;break;
+            case 6 :
+                scoreTeamB = scoreTeamB - 3;displayForTeamB(scoreTeamB);undo_flag = 0;break;
+            default:
+                undo_flag = 0;
+                Toast.makeText(MainActivity.this,"不能再回退了",Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
 }
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 这个项目做过的笔记 http://www.studyjamscn.com/thread-18179-1-1.html
 
 ## JustJava
-一个可以通过按键来调整咖啡数量，然后通过按键来显示总价（每杯5美元）的APP。
+一个可以通过按键来调整咖啡数量，然后通过按键来显示总价（每杯5美元）的 APP。
 
 ## CourtCounter
-一个记录篮球比分的APP。
+一个记录篮球比分的 APP ,轻按 resrt 回退，长按 reset 重置。 
